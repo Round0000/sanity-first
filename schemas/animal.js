aa3685439e23c85701af7c3523d2e7d8cf4463d2
Round0000 <billybinche@gmail.com>
2022-09-09 16:06:46 +0200

    first commit

---
+++ schemas/animal.js
@@ -0,0 +1,24 @@
+export default {
+    name: 'animal',
+    type: 'document',
+      title: 'Animal',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      },
+      {
+        name: 'image',
+        type: 'image',
+        title: 'Image'
+      },
+      {
+        name: 'description',
+        type: 'text',
+        title: 'Description'
+      },
+      
+      
+    ]
+  }
